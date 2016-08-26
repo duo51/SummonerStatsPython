9a5e46470c8b0fb4ccd1403fe8fa0e7876aff440
Xavid <xavidram@hotmail.com>
2016-08-25 21:29:04 -0500

    pushing changes

--- riotPython.py
+++ riotPython.py
@@ -1,37 +1,105 @@
 from requests import *
 import json
 
+
 REGION = "na" #NA by default, can be changed
 URL = "https://" + REGION + ".api.pvp.net"
-APIKEY = ""
+APIKEY = "051db1a2-6b57-41c4-bc99-1e3a29056047"
 RateLimit_perMin = "500" #500 Requests per 10 Minutes
 RateLimit_perSec = "10" #10 requests per second
 
+class Summoner:
 
-def findByUsername(username):
-	# /api/lol/{region}/v1.4/summoner/by-name/{summonerNames}
-	r = get(URL+"/api/lol/" + REGION + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
-	return json.loads(r.text)
+	def __init__(self,username,region):
+		summoner = RiotAPI.findByUsername(username,region)
+		self.username = summoner[username]["name"]
+		self.id = summoner[username]["id"]
+		self.summonderLevel = summoner[username]["summonerLevel"]
+		self.profileIconId = summoner[username]["profileIconId"]
+		self.revsionDate = summoner[username]["revisionDate"]
+		self.region = region
 
-def getSummonerID(username):
-	r = get(URL+"/api/lol/" + REGION + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
-	return json.loads(r.text)[username]["id"]
+	def getMasteries(self):
+		return RiotAPI.getMasteriesById(self.id,self.region)
 
-def getSummonerLevel(username):
-	r = get(URL+"/api/lol/" + REGION + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
-	return json.loads(r.text)[username]["summonerLevel"]
+	def getRunes(self):
+		return RiotAPI.getRunesById(self.id, self.region)
 
-def findBySummonerID(ID):
-	#/api/lol/{region}/v1.4/summoner/{summonerIds}
-	r = get(URL + "/api/lol/" + REGION + "/v1.4/summoner/" + ID + "?api_key=" + APIKEY)
-	return r.text
+class RunePage:
 
-def getMasteries(username):
-	#/api/lol/{region}/v1.4/summoner/{summonerIds}/masteries
-	r = get(URL+"/api/lol/" + REGION + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
-	masteries = get(URL + "/api/lol/" + REGION + "/v1.4/summoner/" + str(json.loads(r.text)[username]["id"]) + "/masteries" + "?api_key=" + APIKEY)
-	return masteries.text
+	def __init__(self,data):
+		self.id = str(data["id"])
+		self.name = data["name"]
+		self.current = data["current"]
+		self.slots = data["slots"]
+
+class MasteryPage:
+
+	def __init__(self,data):
+		self.id = str(data["id"])
+		self.name = data["name"]
+		self.current = data["current"]
+		self.masteries = data["masteries"]
+
+class RiotAPI:
+
+	def findByUsername(username,region):
+		# /api/lol/{region}/v1.4/summoner/by-name/{summonerNames}
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		return json.loads(r.text)
+
+	def getSummonerID(username,region):
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		return json.loads(r.text)[username]["id"]
+
+	def getSummonerLevel(username,region):
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		return json.loads(r.text)[username]["summonerLevel"]
+
+	def getProfileIconId(username,region):
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		return json.loads(r.text)[username]["profileIconId"]
+
+	def getRevisionDate(username,region):
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		return json.loads(r.text)[username]["revisionDate"]
+
+	def findBySummonerID(ID,region):
+		#/api/lol/{region}/v1.4/summoner/{summonerIds}
+		r = get(URL + "/api/lol/" + region + "/v1.4/summoner/" + ID + "?api_key=" + APIKEY)
+		return r.text
+
+	def getMasteriesByUsername(username,region):
+		#/api/lol/{region}/v1.4/summoner/{summonerIds}/masteries
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		masteries = get(URL + "/api/lol/" + region + "/v1.4/summoner/" + str(json.loads(r.text)[username]["id"]) + "/masteries" + "?api_key=" + APIKEY)
+		return masteries.text
+
+	def getMasteriesById(SummonerId,region):
+		#/api/lol/{region}/v1.4/summoner/{summonerIds}/masteries
+		masteries = get(URL + "/api/lol/" + region + "/v1.4/summoner/" + str(SummonerId) + "/masteries" + "?api_key=" + APIKEY)
+		Pages = json.loads(masteries.text)[str(SummonerId)]["pages"]
+		MasteryPages = dict()
+		for page in Pages:
+			MasteryPages[page["name"]] = MasteryPage(page)
+		return MasteryPages
+
+	def getRunesByUsername(username,region):
+		#/api/lol/{region}/v1.4/summoner/{summonerIds}/runes
+		r = get(URL+"/api/lol/" + region + "/v1.4/summoner/by-name/" + username +"?api_key=" + APIKEY)
+		runes = get(URL + "/api/lol/" + region + "/v1.4/summoner/" + str(json.loads(r.text)[username]["id"]) + "/runes" + "?api_key=" + APIKEY)
+		return masteries.text
+
+	def getRunesById(SummonerId,region):
+		#/api/lol/{region}/v1.4/summoner/{summonerIds}/masteries
+		runes = get(URL + "/api/lol/" + region + "/v1.4/summoner/" + str(SummonerId) + "/runes" + "?api_key=" + APIKEY)
+		Pages = json.loads(runes.text)[str(SummonerId)]["pages"]
+		RunePages = dict()
+		for page in Pages:
+			RunePages[page["name"]] = RunePage(page)
+		return RunePages
 
 
 
-print(findByUsername("xavidram") )
+User = Summoner("xavidram","na")
+print(User.getMasteries())
